b8445bafd5bfc582d1003a62827a511a7d14306d
letanatol <letanatol@gmail.com>
2023-08-20 01:52:06 +0200

    feat: add solution brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = [];
+
+  let oneBrackets = bracketsConfig.map(item => item[0]);
+  let twoBrackets = bracketsConfig.map(item => item[1]);
+
+  console.log(oneBrackets, twoBrackets);
+  console.log(str);
+
+  for (let i = 0; i < str.length; i++) {
+    let indexOfLastBracketResult = oneBrackets.indexOf(result.at(-1));
+
+    if (str[i] === twoBrackets[indexOfLastBracketResult]) {
+      result.pop();
+    } else {
+      result.push(str[i]);
+    }
+  }
+
+  return (result.length == 0);
 }
